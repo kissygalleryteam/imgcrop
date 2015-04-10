9c7ac818028dc04ca844fd0dda3b4fc48d1c8ee5
kissyteam <tiehang.lth@taobao.com>
2015-04-10 18:17:34 +0800

    sync daily branch 2.0.1

--- 2.0.1/index.js
+++
@@ -1,9 +0,0 @@
-var ImgCropCanvas = require('./lib/html5/imgcrop');
-var ImgCropNormal = require('./lib/normal/imgcrop');
-
-var Imgcrop = 'getContext' in document.createElement('canvas') ? ImgCropCanvas : ImgCropNormal;
-
-module.exports = Imgcrop;
-
-module.exports.CanvasCrop = ImgCropCanvas;
-module.exports.Crop = ImgCropNormal;
